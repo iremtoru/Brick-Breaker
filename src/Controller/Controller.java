55638240b79ba1a7857a5725cc4447a897214af6
senaodabasi <sena.odabasi@ozu.edu.tr>
2016-12-12 21:13:50 +0200

    Update Controller.java
    I have added new controller options

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -3,7 +3,7 @@ package Controller;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JPanel;
 import View.BrickBreaker;
 import View.HomeScreen;
 
@@ -26,6 +26,17 @@ public class Controller implements ActionListener {
 			cardLayout.show(view.mainPanel, "panel 3");
 		} else if (e.getSource() == view.btnExit) {
 			System.exit(0);
+		} else if (e.getSource() == view.musicSettingButton) {
+			cardLayout.show(view.mainPanel, "panel 4");
+		} else if (e.getSource() == view.speedChangeButton) {
+			cardLayout.show(view.mainPanel, "panel 5");
+		} else if (e.getSource() == view.BGChangeButton) {
+			cardLayout.show(view.mainPanel, "panel 6");
+		} else if (e.getSource() == view.backButton) {
+			cardLayout.show(view.mainPanel, "panel 1");
+		} else if (e.getSource() == view.musicBackBtn || e.getSource() == view.speedBackBtn
+				|| e.getSource() == view.bgBackBtn) {
+			cardLayout.show(view.mainPanel, "panel 2");
 		}
 	}
 }
