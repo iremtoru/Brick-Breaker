fa01f49ded009e180f3ec80e80a36a0082f74c10
senaodabasi <sena.odabasi@ozu.edu.tr>
2016-12-12 21:38:43 +0200

    Update HomeScreen.java
    I added music and back buttons

--- src/View/HomeScreen.java
+++ src/View/HomeScreen.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import Controller.Controller;
+import Model.Music;
+
 import javax.swing.ImageIcon;
 
 public class HomeScreen {
@@ -28,6 +30,7 @@ public class HomeScreen {
 	public JButton backButton;
 	public JButton musicBackBtn;
 	public JButton speedBackBtn;
+	public JButton helpBackBtn;
 	public JButton bgBackBtn;
 	public JButton bgWhiteButton;
 	public JButton bgGrayButton;
@@ -35,6 +38,8 @@ public class HomeScreen {
 	public JButton speedOption1Button;
 	public JButton speedOption2Button;
 	public JButton speedOption3Button;
+	public JButton btnOn;
+	public Music music;
 
 	/**
 	 * Launch the application.
@@ -54,8 +59,8 @@ public class HomeScreen {
 
 	public HomeScreen() {
 		controller = new Controller(this);
-
 		prepareGUI();
+		music = new Music();
 	}
 
 	private void prepareGUI() {
@@ -76,6 +81,7 @@ public class HomeScreen {
 		prepareSpeedChangePanel();
 		prepareBGChangePanel();
 		prepareMainPanel();
+
 	}
 
 	private void prepareButtonPanel() {
@@ -86,27 +92,27 @@ public class HomeScreen {
 		btnStart.setForeground(new Color(0, 0, 0));
 		btnStart.setBackground(new Color(240, 128, 128));
 		btnStart.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		btnStart.setBounds(245, 101, 150, 48);
-		btnStart.setSize(150, 48);
+		btnStart.setBounds(240, 150, 150, 30);
 
 		btnSettings = new JButton("Settings");
 		btnSettings.addActionListener(controller);
 		btnSettings.setBackground(new Color(255, 255, 224));
 		btnSettings.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		btnSettings.setBounds(245, 166, 150, 48);
+		btnSettings.setBounds(240, 200, 150, 30);
 
 		btnHelpMenu = new JButton("Help Menu");
 		btnHelpMenu.addActionListener(controller);
 		btnHelpMenu.setForeground(new Color(0, 0, 0));
 		btnHelpMenu.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 		btnHelpMenu.setBackground(new Color(255, 222, 173));
-		btnHelpMenu.setBounds(245, 235, 150, 48);
+		btnHelpMenu.setBounds(240, 250, 150, 30);
 
 		btnExit = new JButton("Exit");
 		btnExit.addActionListener(controller);
 		btnExit.setBackground(new Color(224, 255, 255));
 		btnExit.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
-		btnExit.setBounds(245, 300, 150, 48);
+		btnExit.setBounds(240, 300, 150, 30);
+		buttonPanel.setLayout(null);
 
 		buttonPanel.add(btnStart);
 		buttonPanel.add(btnSettings);
@@ -115,7 +121,6 @@ public class HomeScreen {
 	}
 
 	private void prepareSettingsPanel() {
-		// add contents of the panel
 		settingsPanel = new JPanel();
 
 		musicSettingButton = new JButton("Music Settings");
@@ -137,7 +142,7 @@ public class HomeScreen {
 		BGChangeButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
 
 		JLabel s = new JLabel("SETTINGS");
-		s.setBounds(294, 100, 50, 20);
+		s.setBounds(294, 100, 70, 20);
 
 		backButton = new JButton("<-Back");
 		backButton.setBounds(10, 380, 150, 30);
@@ -159,74 +164,105 @@ public class HomeScreen {
 	}
 
 	private void prepareMusicSettingPanel() {
-		// TODO Auto-generated method stub
 		musicSettingPanel = new JPanel();
-		JLabel music = new JLabel("music");
-		musicSettingPanel.add(music);
-		
+		musicSettingPanel.setLayout(null);
+
+		JLabel musicLabel = new JLabel("Music On/Off");
+		musicLabel.setBounds(278, 99, 110, 20);
+
 		musicBackBtn = new JButton("<-Back");
 		musicBackBtn.setBounds(10, 380, 150, 30);
 		musicBackBtn.addActionListener(controller);
 		musicBackBtn.setBackground(new Color(204, 255, 255));
+
+		btnOn = new JButton("On");
+		btnOn.addActionListener(controller);
+		btnOn.setBackground(new Color(240, 128, 128));
+		btnOn.setBounds(240, 150, 150, 30);
+
+		musicSettingPanel.add(musicLabel);
+		musicSettingPanel.add(btnOn);
 		musicSettingPanel.add(musicBackBtn);
 	}
 
 	private void prepareSpeedChangePanel() {
-		// TODO Auto-generated method stub
 		speedChangePanel = new JPanel();
-		JLabel speed = new JLabel("speed");
+		speedChangePanel.setLayout(null);
+		JLabel speed = new JLabel("Speed");
+		speed.setBounds(294, 100, 70, 20);
 		speedChangePanel.add(speed);
-		
+
 		speedBackBtn = new JButton("<-Back");
 		speedBackBtn.setBounds(10, 380, 150, 30);
 		speedBackBtn.addActionListener(controller);
 		speedBackBtn.setBackground(new Color(204, 255, 255));
 		speedChangePanel.add(speedBackBtn);
-		
+
 		speedOption1Button = new JButton("Speed: 1.5");
+		speedOption1Button.setBackground(new Color(240, 128, 128));
+		speedOption1Button.setBounds(240, 150, 150, 30);
 		speedOption1Button.addActionListener(controller);
 		speedChangePanel.add(speedOption1Button);
-		
+
 		speedOption2Button = new JButton("Speed: 2");
+		speedOption2Button.setBackground(new Color(240, 128, 128));
+		speedOption2Button.setBounds(240, 200, 150, 30);
 		speedOption2Button.addActionListener(controller);
 		speedChangePanel.add(speedOption2Button);
-		
+
 		speedOption3Button = new JButton("Speed: 2.5");
+		speedOption3Button.setBackground(new Color(240, 128, 128));
+		speedOption3Button.setBounds(240, 250, 150, 30);
 		speedOption3Button.addActionListener(controller);
 		speedChangePanel.add(speedOption3Button);
 	}
 
 	private void prepareBGChangePanel() {
-		// TODO Auto-generated method stub
 		BGChangePanel = new JPanel();
-		JLabel bg = new JLabel("bg");
+		BGChangePanel.setLayout(null);
+		JLabel bg = new JLabel("Background");
+		bg.setBounds(294, 100, 100, 20);
 		BGChangePanel.add(bg);
-		
+
 		bgBackBtn = new JButton("<-Back");
+		bgBackBtn.setBackground(new Color(204, 255, 255));
 		bgBackBtn.setBounds(10, 380, 150, 30);
 		bgBackBtn.addActionListener(controller);
 		BGChangePanel.add(bgBackBtn);
-		
+
 		bgWhiteButton = new JButton("White");
+		bgWhiteButton.setBounds(240, 150, 150, 30);
+		bgWhiteButton.setBackground(new Color(255, 255, 255));
 		bgWhiteButton.addActionListener(controller);
 		BGChangePanel.add(bgWhiteButton);
-		
+
 		bgGrayButton = new JButton("Gray");
+		bgGrayButton.setBackground(new Color(192, 192, 192));
+		bgGrayButton.setBounds(240, 200, 150, 30);
 		bgGrayButton.addActionListener(controller);
 		BGChangePanel.add(bgGrayButton);
-		
+
 		bgPinkButton = new JButton("Pink");
+		bgPinkButton.setBackground(new Color(255, 105, 180));
+		bgPinkButton.setBounds(240, 250, 150, 30);
 		bgPinkButton.addActionListener(controller);
 		BGChangePanel.add(bgPinkButton);
 	}
 
 	private void prepareHelpMenuPanel() {
-		// add contents of the panel
 		helpMenuPanel = new JPanel();
+		helpMenuPanel.setLayout(null);
 
 		JLabel helpInfo = new JLabel("Help Info goes here.");
-
+		helpInfo.setBounds(230, 100, 200, 20);
 		helpMenuPanel.add(helpInfo);
+
+		helpBackBtn = new JButton("<-Back");
+		helpBackBtn.setBackground(new Color(204, 255, 255));
+		helpBackBtn.setBounds(10, 380, 150, 30);
+		helpBackBtn.addActionListener(controller);
+		helpMenuPanel.add(helpBackBtn);
+
 	}
 
 	private void prepareMainPanel() {
